b20b1bc56c4bc7ce10bb96c89b7ac4a9365b4597
Ghasem Ramezani <g1999ramezani@gmail.com>
2026-04-27 20:08:23 +0330

    [#] //
    Signed-off-by: Ghasem Ramezani <g1999ramezani@gmail.com>

---
+++ proton-cachyos-10.0-20260424-slr-x86_64/user_settings.py
@@ -0,0 +1,17 @@
+user_settings = {
+    #
+    # Enable logging
+    #
+    "PROTON_LOG": "1",
+
+    #
+    # Logs level
+    #
+    "WINEDEBUG"           : "+timestamp,+pid,+tid,+seh,+unwind,+threadname,+debugstr,+loaddll,+mscoree",
+    "DXVK_LOG_LEVEL"      : "info",
+    "DXVK_NVAPI_LOG_LEVEL": "info",
+    "VKD3D_DEBUG"         : "warn",
+    "VKD3D_SHADER_DEBUG"  : "fixme",
+    "WINE_MONO_TRACE"     : "E:System.NotImplementedException",
+    "MONO_LOG_LEVEL"      : "info"
+}
